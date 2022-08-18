108a9069b113c994e15e95f207fbbfc870ffaad1
abusayed22 <abusayed.in22@gmail.com>
2022-08-18 22:37:49 +0530

    ok

--- script.js
+++ script.js
@@ -1,4 +1,125 @@
-<div id="main" class="mx-auto max-w-md mt-10 space-y-5">
+// dom element
+const counterEl = document.getElementById("counter");
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById("decrement");
+const addCounterEl = document.getElementById("addCounter");
+const resetEl = document.getElementById("reset");
+
+
+
+
+// create idetnify
+const INCREMENT = "increment"
+const DECREMENT = "decrement"
+const ADDCOMPONENT = "addcomponent"
+const RESET = "reset"
+
+
+// let intioastate
+
+const intialState = {
+    value: 0
+}
+
+// create reducer 
+const counterReducer = (state = intialState, action) => {
+    switch (action.type) {
+
+        case INCREMENT: return {
+            ...state,
+            value: state.value + action.payload
+        }
+
+            break;
+
+        case DECREMENT: return {
+            ...state,
+            value: state.value - action.payload
+        }
+            break;
+
+        case ADDCOMPONENT: return {
+            ...state,
+            comStatus: true
+        }
+            break;
+
+        case RESET: return {
+            ...state,
+            value: action.payload
+        }
+            break;
+
+        default:
+            return state
+            break;
+    }
+}
+
+// create store 
+const store = Redux.createStore(counterReducer)
+
+// test
+
+// action triger 
+const incrementHandler = (value) => {
+    return {
+        type: INCREMENT,
+        payload: value
+    }
+}
+const decrementHandler = (value) => {
+    return {
+        type: DECREMENT,
+        payload: value
+    }
+}
+const resetFunc = (value) => {
+    return {
+        type: RESET,
+        payload: value,
+
+    }
+}
+
+
+const render = () => {
+    const state = store.getState()
+    counterEl.innerText = state.value
+}
+
+// subscribe
+store.subscribe(render)
+
+render()
+
+
+
+
+// event listiner 
+incrementEl.addEventListener('click', () => {
+    store.dispatch(incrementHandler(2))
+})
+
+decrementEl.addEventListener('click', () =>
+    store.dispatch(decrementHandler(2)))
+
+resetEl.addEventListener('click', () =>
+    store.dispatch(resetFunc(0))
+)
+
+let count = 0;
+
+addCounterEl.addEventListener('click', () => {
+   ++count
+   const fakearr = [1,2,3]
+
+    const mainEl = document.getElementById("main");
+    const readingEl = document.getElementById("pushing");
+    
+
+    readingEl.innerHTML = `
+    ${fakearr.map(d => `<div id="main" class="mx-auto max-w-md mt-10 space-y-5">
     <div
         class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
     >
@@ -18,4 +139,15 @@
             </button>
         </div>
     </div>
-</div>
+</div>`)}
+    
+    `
+    // readingEl.appendChild(testArr[0]);
+    // readingEl.appendChild(testArr[1]);
+
+    // readingEl.appendChild(mainEl)
+    // console.log(mainEl);
+    console.log('testing',count)
+  
+})
+console.log(count)
